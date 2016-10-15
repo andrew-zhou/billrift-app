e34a3fa52e32353289f4d5225e25ff1872f7c1a4
zuqini <lizuqiliang@gmail.com>
2016-10-15 18:01:32 -0400

    Transaction model

--- app/src/main/java/com/BillRift/models/Transaction.java
+++ app/src/main/java/com/BillRift/models/Transaction.java
@@ -5,5 +5,40 @@ package com.BillRift.models;
  */
 
 public class Transaction {
-    // TODO: @SerializedName
+    private String from;
+    private String to;
+    private double amount;
+    private String title;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
