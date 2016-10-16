7bf5bdd5e5f5d9fb66d27fb82bbcd026768c9b0e
zuqini <lizuqiliang@gmail.com>
2016-10-15 20:29:22 -0400

    Fix mvp fragment again

--- app/src/main/java/com/BillRift/MvpFragment.java
+++ app/src/main/java/com/BillRift/MvpFragment.java
@@ -1,8 +1,9 @@
 package com.BillRift;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
 
 import com.BillRift.presenters.BasePresenter;
 
@@ -15,21 +16,22 @@ public abstract class MvpFragment<P extends BasePresenter> extends Fragment {
         if (savedInstanceState == null) {
             presenter = createPresenter();
         } else {
-            PresenterManager.getInstance().restorePresenter(savedInstanceState);
+            presenter = PresenterManager.getInstance().restorePresenter(savedInstanceState);
         }
     }
+
 
     @Override
     public void onResume() {
         super.onResume();
+
         presenter.bindView(this);
     }
 
     @Override
-    public void onDestroy() {
-        super.onPause();
-        Bundle bundle = new Bundle();
-        PresenterManager.getInstance().savePresenter(presenter, bundle);
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        PresenterManager.getInstance().savePresenter(presenter, outState);
         presenter.unbindView();
     }
 
