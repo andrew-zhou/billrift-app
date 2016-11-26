e36fbb0fe28b33bad9690e90fa91cca711c79145
Andrew Zhou <mrehzee@gmail.com>
2016-11-26 14:54:56 -0500

    unit tests are garbage

--- app/src/test/java/com/BillRift/TransactionPresenterTest.java
+++ app/src/test/java/com/BillRift/TransactionPresenterTest.java
@@ -16,10 +16,11 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.easymock.EasyMock.expect;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Log.class, UserDatabase.class})
@@ -34,7 +35,7 @@ public class TransactionPresenterTest {
         fakeModel = Mockito.mock(Transaction.class);
         presenter = new TransactionPresenter(fakeModel);
         view = Mockito.mock(TransactionView.class);
-        PowerMockito.mockStatic(Log.class);
+        mockStatic(Log.class);
         presenter.bindView(view);
     }
 
@@ -45,20 +46,18 @@ public class TransactionPresenterTest {
 
     @Test
     public void testUpdateView_withModel_setsText() {
-        PowerMockito.mockStatic(UserDatabase.class);
+        mockStatic(UserDatabase.class);
+
         fakeDB = Mockito.mock(UserDatabase.class);
+        Mockito.when(fakeDB.getUser("1")).thenReturn(new User("Fake", "Fake", "1"));
+        Mockito.when(fakeDB.getUser("2")).thenReturn(new User("Fake", "Fake", "2"));
+        Mockito.when(fakeModel.getFrom()).thenReturn("1");
+        Mockito.when(fakeModel.getTo()).thenReturn("2");
+        when(UserDatabase.getInstance()).thenReturn(fakeDB);
 
         reset(view);
-
-        expect(UserDatabase.getInstance()).andReturn(fakeDB);
-
         presenter.bindView(view);
 
-
-        expect(fakeDB.getUser("1")).andReturn(new User("Fake", "Fake", "1"));
-        expect(fakeDB.getUser("2")).andReturn(new User("Fake", "Fake", "2"));
-        expect(fakeModel.getFrom()).andReturn("1");
-        expect(fakeModel.getTo()).andReturn("2");
         verify(view).setText(Mockito.anyString());
     }
 }
